e05ecf4792e6d89bbb6ae211c0efb070507eafff
Minh-An Quinn <minhanquinn@DN51usp4.SUNet>
2018-05-27 18:18:47 -0700

    plots

--- utils/score.py
+++ utils/score.py
@@ -2,6 +2,9 @@
 #Original credit - @bgalbraith
 
 import sklearn
+import itertools
+import numpy as np
+import matplotlib.pyplot as plt
 
 #LABELS = ['agree', 'disagree', 'discuss', 'unrelated']
 #LABELS_RELATED = ['unrelated','related']
@@ -58,6 +61,41 @@ def print_confusion_matrix(cm):
         lines.append("-"*line_len)
     print('\n'.join(lines))
 
+# our new confusion matrix pretty print
+def confusion_matrix_pretty_print(cm, classes,
+                          normalize=False,
+                          title='Confusion matrix',
+                          cmap=plt.cm.Blues):
+    """
+    This function prints and plots the confusion matrix.
+    Normalization can be applied by setting `normalize=True`.
+    """
+    if normalize:
+        cm = cm.astype('float') / cm.sum(axis=1)[:, np.newaxis]
+        print("Normalized confusion matrix")
+    else:
+        print('Confusion matrix, without normalization')
+
+    print(cm)
+
+    plt.imshow(cm, interpolation='nearest', cmap=cmap)
+    plt.title(title)
+    plt.colorbar()
+    tick_marks = np.arange(len(classes))
+    plt.xticks(tick_marks, classes, rotation=45)
+    plt.yticks(tick_marks, classes)
+
+    fmt = '.2f' if normalize else 'd'
+    thresh = cm.max() / 2.
+    for i, j in itertools.product(range(cm.shape[0]), range(cm.shape[1])):
+        plt.text(j, i, format(cm[i, j], fmt),
+                 horizontalalignment="center",
+                 color="white" if cm[i, j] > thresh else "black")
+
+    plt.tight_layout()
+    plt.ylabel('True label')
+    plt.xlabel('Predicted label')
+
 
 def report_score(actual,predicted):
     score,cm = score_submission(actual,predicted)
@@ -65,7 +103,20 @@ def report_score(actual,predicted):
     best_score = len(actual)
 
     #print_confusion_matrix(cm)
-    print(sklearn.metrics.confusion_matrix(actual, predicted))
+    cnf_matrix = sklearn.metrics.confusion_matrix(actual, predicted)
+    np.set_printoptions(precision=2)
+    # Plot non-normalized confusion matrix
+    plt.figure()
+    confusion_matrix_pretty_print(cnf_matrix, classes=['agree', 'disagree', 'discuss'],
+                          title='Confusion matrix, without normalization')
+
+    # Plot normalized confusion matrix
+    plt.figure()
+    confusion_matrix_pretty_print(cnf_matrix, classes=['agree', 'disagree', 'discuss'], normalize=True,
+                          title='Normalized confusion matrix')
+
+    plt.show()
+
     print("Score: " +str(score) + " out of " + str(best_score) + "\t("+str(score*100/best_score) + "%)")
     #return -1 # no score for binary
     return score*100/best_score
